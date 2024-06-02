d96df20517d8d5d240f64e8287efe247a2ac6aeb
lobYno <163894923+lobYno@users.noreply.github.com>
2024-06-02 21:58:00 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,1 +1,14 @@
+<?php
+session_start();
 
+$servername = "localhost";
+$username = "userdb";
+$password = "databaza";
+$dbname = "northwind";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
